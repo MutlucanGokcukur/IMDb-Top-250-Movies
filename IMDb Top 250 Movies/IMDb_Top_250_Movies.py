82da7541b2f6d8d377de20c3f36f27f0afa26779
MSI <MSI@MUTLU-PC>
2023-01-28 14:08:21 +0300

    Proje dosyası ekle.

---
+++ IMDb Top 250 Movies/IMDb_Top_250_Movies.py
@@ -0,0 +1,26 @@
+﻿import requests
+from bs4 import BeautifulSoup
+
+url="https://www.imdb.com/chart/top/"
+
+response=requests.get(url)
+
+html=response.content
+
+soup=BeautifulSoup(html,"html.parser")  #URL üzerindeki her şeyi buraya aldık.
+
+baslıklar=soup.find_all("td",{"class":"titleColumn"})   #İncele kısmından elde edilen başlıklar ile film isimlerini aldık.
+reytingler=soup.find_all("td",{"class":"ratingColumn imdbRating"})
+
+for baslik , reyting in zip(baslıklar,reytingler):
+    baslik=baslik.text
+    reyting=reyting.text
+    baslik=baslik.strip()               #İlk ve son kısımları sildik.
+    baslik=baslik.replace("\n","")      #Alt satıra geçmeleri iptal ettik
+
+    reyting=reyting.strip()
+    reyting=reyting.replace("\n","")
+
+    print("Film Adı: ",baslik)
+    print("Reyting: ",reyting)
+
